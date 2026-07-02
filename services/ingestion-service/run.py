6756626a5b5d59335a66a12aa93b2ed0a73436b6
Vaibhav Dike <vaibhav.dike@accelirate.com>
2026-07-02 21:08:32 +0530

    Implement settings configuration

--- services/ingestion-service/run.py
+++ services/ingestion-service/run.py
@@ -1,5 +1,16 @@
-from app.main import main
+from app.config.settings import settings
 
+print("=" * 50)
+print("Application :", settings.APP_NAME)
+print("Environment :", settings.ENV)
 
-if __name__ == "__main__":
-    main()
+print("\nAWS")
+print(settings.AWS_BUCKET_NAME)
+print(settings.AWS_REGION)
+
+print("\nSnowflake")
+print(settings.SNOWFLAKE_ACCOUNT)
+print(settings.SNOWFLAKE_DATABASE)
+print(settings.SNOWFLAKE_WAREHOUSE)
+
+print("=" * 50)
