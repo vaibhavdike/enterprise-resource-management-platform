7b9d4018f314b11a936b3ddbf4fa18b6b1f21f85
Vaibhav Dike <vaibhav.dike@accelirate.com>
2026-07-03 18:02:52 +0530

    Add image name

--- infrastructure/services/ingestion-service.sql
+++ infrastructure/services/ingestion-service.sql
@@ -1,5 +1,37 @@
-USE ROLE ACCOUNTADMIN;
+-- ==========================================================
+-- Use Database, Schema and Role
+-- ==========================================================
 
-CREATE IMAGE REPOSITORY IF NOT EXISTS IRM_IMAGE_REPOSITORY;
+USE ROLE SYSADMIN;
+USE DATABASE IRM_DB;
+USE SCHEMA RAW;
 
-SHOW IMAGE REPOSITORIES;
+-- ==========================================================
+-- Create / Replace Snowpark Container Service
+-- ==========================================================
+
+CREATE OR REPLACE SERVICE INGESTION_SERVICE
+IN COMPUTE POOL IRM_COMPUTE_POOL
+FROM SPECIFICATION $$
+spec:
+  containers:
+    - name: ingestion-service
+
+      image: a4357138117071-accelirate-partner.registry.snowflakecomputing.com/irm_db/raw/irm_image_repository:latest
+
+      command:
+        - python
+
+      args:
+        - run.py
+
+      env:
+        APP_NAME: "Ingestion Service"
+        ENV: "DEV"
+        LOG_LEVEL: "INFO"
+
+  endpoints:
+    - name: ingestion
+      port: 8080
+      public: false
+$$;
