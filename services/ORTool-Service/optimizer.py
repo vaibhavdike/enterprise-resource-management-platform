27963555e4c97bfa3410b4aba7786023b3dd78a8
Vaibhav Dike <vaibhav.dike@accelirate.com>
2026-07-21 16:13:47 +0530

    or tool update

--- services/ORTool-Service/optimizer.py
+++ services/ORTool-Service/optimizer.py
@@ -48,32 +48,32 @@ from datetime import datetime
 
 # for local development
 
-connection_parameters = {
-    "account": "A4357138117071-ACCELIRATE_PARTNER",
-    "user": "VAIBHAVDIKE",
-    "password": "Vaibhav@123456789",
-    "role": "SYSADMIN",
-    "warehouse": "DEMO_WH",
-    "database": "IRM_DATABASE",
-    "schema": "OPERATIONAL"
-}
+# connection_parameters = {
+#     "account": "A4357138117071-ACCELIRATE_PARTNER",
+#     "user": "VAIBHAVDIKE",
+#     "password": "Vaibhav@123456789",
+#     "role": "SYSADMIN",
+#     "warehouse": "DEMO_WH",
+#     "database": "IRM_DATABASE",
+#     "schema": "OPERATIONAL"
+# }
 
-session = Session.builder.configs(connection_parameters).create()
+# session = Session.builder.configs(connection_parameters).create()
 
 
 # credentials for production
 
-# connection_params = {
-#     "host": os.environ["SNOWFLAKE_HOST"],
-#     "account": os.environ["SNOWFLAKE_ACCOUNT"],
-#     "authenticator": "oauth",
-#     "token": open("/snowflake/session/token").read().strip(),
-#     "warehouse": "DEMO_WH",
-#     "database": "IRM_DB",
-#     "schema": "OPERATIONAL"
-# }
+connection_params = {
+    "host": os.environ["SNOWFLAKE_HOST"],
+    "account": os.environ["SNOWFLAKE_ACCOUNT"],
+    "authenticator": "oauth",
+    "token": open("/snowflake/session/token").read().strip(),
+    "warehouse": "DEMO_WH",
+    "database": "IRM_DB",
+    "schema": "OPERATIONAL"
+}
 
-# session = Session.builder.configs(connection_params).create()
+session = Session.builder.configs(connection_params).create()
 
 print("=" * 70)
 print("Connected to Snowflake Successfully")
